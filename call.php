437be5fd10345df87d4c4db7ef65d6b68d72d7c6
akhmadarief <38371717+akhmadarief@users.noreply.github.com>
2019-09-27 14:50:42 +0700

    Update call.php

--- call.php
+++ call.php
@@ -1,34 +1,133 @@
 <?php
+$db_host = 'localhost'; // Nama Server
+$db_user = 'root'; // User Server
+$db_pass = ''; // Password Server
+$db_name = 'yo'; // Nama Database
 
-$conn = new mysqli("localhost", "root", "", "yo");
-$sql = "SELECT id, full_name, email FROM register";
-    $result = $conn-> query($sql);
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+if (!$conn) {
+	die ('Gagal terhubung dengan MySQL: ' . mysqli_connect_error());	
+}
 
-    if ($result-> num_rows > 0) {
-      echo "<table>
-      <tr>
-      <th> ID </th>
-      <th>Nama</th>
-      <th>Email</th>";
-      while ($row = $result-> fetch_assoc()) {
-        echo "<tr><td>". $row["id"] ."</td><td>". $row["full_name"] . "</td><td>". $row["email"] ."</td></tr>";
-      }
-      echo "</table>";
-    }
-    else {
-      echo "0 result";
-    }
+$sql = 'SELECT * 
+		FROM register';
+		
+$query = mysqli_query($conn, $sql);
 
-    $referer = filter_var($_SERVER['HTTP_REFERER'], FILTER_VALIDATE_URL);
+if (!$query) {
+	die ('SQL Error: ' . mysqli_error($conn));
+}
+?>
+<html>
+<head>
+	<title>Data Peserta Seminar</title>
+	<style type="text/css">
+		body {
+			font-size: 15px;
+			color: #343d44;
+			font-family: "segoe-ui", "open-sans", tahoma, arial;
+			padding: 0;
+			margin: 0;
+		}
+		table {
+			margin: auto;
+			font-family: "Lucida Sans Unicode", "Lucida Grande", "Segoe Ui";
+			font-size: 12px;
+		}
 
+		h1 {
+			margin: 25px auto 0;
+			text-align: center;
+			text-transform: uppercase;
+			font-size: 17px;
+		}
 
-  if (!empty($referer)) {
-    
-    echo '<p><a href="'. $referer .'" title="Return to the previous page">&laquo; Go back</a></p>';
-    
-  } else {
-    
-    echo '<p><a href="javascript:history.go(-1)" title="Return to the previous page">&laquo; Go back</a></p>';
-    
-  }
-    ?>
+		table td {
+			transition: all .5s;
+		}
+		
+		/* Table */
+		.data-table {
+			border-collapse: collapse;
+			font-size: 14px;
+			min-width: 537px;
+		}
+
+		.data-table th, 
+		.data-table td {
+			border: 1px solid #e1edff;
+			padding: 7px 17px;
+		}
+		.data-table caption {
+			margin: 7px;
+		}
+
+		/* Table Header */
+		.data-table thead th {
+			background-color: #508abb;
+			color: #FFFFFF;
+			border-color: #6ea1cc !important;
+			text-transform: uppercase;
+		}
+
+		/* Table Body */
+		.data-table tbody td {
+			color: #353535;
+		}
+		.data-table tbody td:first-child,
+		.data-table tbody td:nth-child(4),
+		.data-table tbody td:last-child {
+			text-align: right;
+		}
+
+		.data-table tbody tr:nth-child(odd) td {
+			background-color: #f4fbff;
+		}
+		.data-table tbody tr:hover td {
+			background-color: #ffffa2;
+			border-color: #ffff0f;
+		}
+
+		/* Table Footer */
+		.data-table tfoot th {
+			background-color: #e5f5ff;
+			text-align: right;
+		}
+		.data-table tfoot th:first-child {
+			text-align: left;
+		}
+		.data-table tbody td:empty
+		{
+			background-color: #ffcccc;
+		}
+	</style>
+</head>
+<body>
+	<table class="data-table">
+		<caption class="title">Data Peserta</caption>
+		<thead>
+			<tr>
+				<th>No</th>
+				<th>ID</th>
+				<th>Nama</th>
+				<th>Email</th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php
+		$no 	= 1;
+		$total 	= 0;
+		while ($row = mysqli_fetch_array($query))
+		{
+			echo '<tr>
+					<td>'.$no.'</td>
+					<td>'.$row['id'].'</td>
+					<td>'.$row['full_name'].'</td>
+					<td>'.$row['email'].'</td>
+				</tr>';
+			$no++;
+		}?>
+		</tbody>
+	</table>
+</body>
+</html>
